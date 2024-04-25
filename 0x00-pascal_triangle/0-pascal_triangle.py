f1e23e5073d6f5e0b26eebaacd3b72d6c7a2a036
Tony Mputhia <tonymmputhia@gmail.com>
2024-04-26 00:29:12 +0300

    Fix pycodestyle errors

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,6 +4,7 @@ This module comes up with Pascal's triangle
 
 """
 
+
 def pascal_triangle(n):
     """
     Returns a list of lists of integers representing Pascal's triangle.
@@ -22,6 +23,4 @@ def pascal_triangle(n):
             row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
         row.append(1)
         triangle.append(row)
-    
     return triangle
-
