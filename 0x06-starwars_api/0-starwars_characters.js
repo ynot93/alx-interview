43901f77c9bca923282318bd4977751b9108d3c1
Tony Mputhia <tonymmputhia@gmail.com>
2024-06-07 05:39:51 +0300

    Fix semistandard errors

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,44 +2,26 @@
 
 const request = require('request');
 
-const movieId = process.argv[2];
+const filmNum = process.argv[2] + '/';
+const filmURL = 'https://swapi-api.hbtn.io/api/films/';
 
-// Check if the movie ID is provided
-if (!movieId) {
-  console.error('Please provide a movie ID');
-  process.exit(1);
-}
-
-const filmUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-// Make the request to get the movie details
 request(filmURL + filmNum, async function (err, res, body) {
-    if (err) {
-        console.error('Error fetching character data:', err);
-        return;
-    }
+  if (err) {
+    return console.error(err);
+  }
 
-    if (res.statusCode !== 200) {
-        console.error('Failed to fetch character data, status code:', res.statusCode);
-        return;
-    }
-    const charUrls = JSON.parse(body).characters;
+  const charURLList = JSON.parse(body).characters;
 
-    for (const charURL of charUrls) {
-      await new Promise(function (resolve, reject) {
-        request(charURL, function (err, res, body) {
-					if (err) {
-            console.error('Error fetching character data:', err);
-            return;
-          }
-        
-					if (res.statusCode !== 200) {
-						console.error('Failed to fetch character data, status code:', res.statusCode);
-						return;
-					}
-          console.log(JSON.parse(body).name);
-          resolve();
-        });
+  for (const charURL of charURLList) {
+    await new Promise(function (resolve, reject) {
+      request(charURL, function (err, res, body) {
+        if (err) {
+          return console.error(err);
+        }
+
+        console.log(JSON.parse(body).name);
+        resolve();
       });
-    }
-  });
+    });
+  }
+});
